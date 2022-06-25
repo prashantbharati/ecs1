2ff46c872961f075f882ecc9b396d79c3a617f14
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-25 19:10:10 +0530

    top half done

--- src/forms/fileimport.js
+++ src/forms/fileimport.js
@@ -1,6 +1,12 @@
+import { Grid, TextField, Typography } from "@mui/material";
 import React from "react";
 import Tabel from "../tabel.js";
 
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
 const fields = [
   "Date",
   "File Name",
@@ -15,7 +21,86 @@ const fields = [
 const fileimport = () => {
   return (
     <>
-      <Tabel fields={fields} />
+      <div style={{ height: "40vh" }}>
+        <Grid container>
+          <Grid item xs={3}>
+            <div
+              style={{
+                height: "40vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Typography
+                style={{
+                  fontFamily: "Poppins",
+                  fontSize: "20px",
+                  marginLeft: "12px",
+                  color: "#596278",
+                }}
+              >
+                Search File Name
+              </Typography>
+              <TextField
+                name="table"
+                fullWidth
+                variant="outlined"
+                style={{
+                  width: "90%",
+                  marginLeft: "10px",
+
+                  paddingBottom: 0,
+                  marginTop: 0,
+                  fontWeight: 500,
+                }}
+                label="Search File Name"
+              />
+            </div>
+          </Grid>
+
+          <Grid item xs={3}>
+            <div
+              style={{
+                height: "40vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Typography
+                style={{
+                  fontFamily: "Poppins",
+                  fontSize: "20px",
+                  marginLeft: "12px",
+                  color: "#596278",
+                }}
+              >
+                Filter
+              </Typography>
+              <Box style={{ width: "100%" }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">
+                    Add a Filter
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label="query"
+                  >
+                    <MenuItem value={1}>Status</MenuItem>
+                    <MenuItem value={2}>RTA</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+
+      <div style={{ height: "60vh", margin: "10px" }}>
+        <Tabel fields={fields} />
+      </div>
     </>
   );
 };

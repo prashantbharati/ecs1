ad10c94444c2f0951543eb96e067df590ba36a2e
prashantbharati92@gmail.com <prashantbharati92@gmail.com>
2022-06-26 08:31:43 +0530

    create transaction done

---
+++ src/forms/createtransaction.js
@@ -0,0 +1,177 @@
+import { Button, Grid, Typography } from "@mui/material";
+import React from "react";
+import Tabel from "../tabel.js";
+
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFilter, faUser } from "@fortawesome/free-solid-svg-icons";
+import "antd/es/date-picker/style/css";
+
+import { DatePicker } from "antd";
+const { RangePicker } = DatePicker;
+
+const fields = [
+  "Date",
+  "Scheme",
+  "Folio",
+  "Applicant",
+  "Transaction Type",
+  "Purchase Price",
+  "Download",
+  "Units",
+  "Amount",
+];
+
+const fileimport = () => {
+  return (
+    <>
+      <div style={{ height: "40vh" }}>
+        <Grid container>
+          <Grid item xs={3}>
+            <div
+              style={{
+                height: "40vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Typography
+                style={{
+                  fontFamily: "Poppins",
+                  fontSize: "20px",
+                  marginLeft: "12px",
+                  color: "#596278",
+                }}
+              >
+                ARN
+              </Typography>
+              <Box style={{ width: "100%", marginLeft: "12px" }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">
+                    {
+                      <FontAwesomeIcon
+                        className="txicon"
+                        style={{ height: "20px" }}
+                        icon={faFilter}
+                      />
+                    }
+                    {"         "}
+                    Add a Filter
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label="query"
+                  >
+                    <MenuItem value={1}>ARN-1668</MenuItem>
+                    <MenuItem value={2}>ARN-00000</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+            </div>
+          </Grid>
+
+          <Grid item xs={2}></Grid>
+
+          <Grid item xs={4}>
+            <div
+              style={{
+                height: "40vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                marginRight: "12px",
+              }}
+            >
+              <RangePicker picker="week" />
+            </div>
+          </Grid>
+
+          <Grid item xs={3}>
+            <div
+              style={{
+                height: "40vh",
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+              }}
+            >
+              <Box
+                style={{
+                  width: "100%",
+                  marginTop: "12px",
+                  backgroundColor: "#4579e3",
+                }}
+              >
+                <FormControl fullWidth>
+                  <InputLabel
+                    id="demo-simple-select-label"
+                    style={{ color: "white" }}
+                  >
+                    {
+                      <FontAwesomeIcon
+                        className="txicon"
+                        style={{ height: "20px", color: "white" }}
+                        icon={faUser}
+                      />
+                    }
+                    {"         "}
+                    Select User(s)
+                  </InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    label="query"
+                  >
+                    <MenuItem value={1}>Relationship Manager</MenuItem>
+                    <MenuItem value={2}>Sub Broker</MenuItem>
+                    <MenuItem value={2}>Family Head</MenuItem>
+                    <MenuItem value={2}>Client</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+            </div>
+          </Grid>
+        </Grid>
+      </div>
+
+      <div>
+        <Grid container>
+          <Grid item xs={2} style={{ marginLeft: "20px" }}>
+            Mutual Fund
+          </Grid>
+          <Grid item xs={2} style={{}}>
+            Share & Bond
+          </Grid>
+          <Grid item xs={2} style={{}}>
+            Fixed Deposit
+          </Grid>
+          <Grid item xs={2} style={{}}>
+            Other Assets
+          </Grid>
+          <Grid item xs={1}></Grid>
+          <Grid item xs={2}>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ width: "90%", height: "100%" }}
+            >
+              Add Transaction
+            </Button>
+          </Grid>
+        </Grid>
+      </div>
+
+      <div style={{ height: "60vh", margin: "10px" }}>
+        <Tabel fields={fields} />
+      </div>
+    </>
+  );
+};
+
+export default fileimport;
